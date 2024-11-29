8c692cde749eede1db4877dd8cbc1e82b4e7fe6a
Mvittorelli <mvittorelli@edenschool.fr>
2024-11-29 16:26:27 +0100

    feat(app): cat

---
+++ script.js
@@ -0,0 +1,19 @@
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eigth = document.getElementById("eigth");
+const nine = document.getElementById("nine");
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const time = document.getElementById("time");
+
+const reset = document.getElementById("reset");
+const equal = document.getElementById("equal");
+
+let numbers = []
